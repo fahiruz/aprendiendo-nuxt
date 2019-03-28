89791b38df669efe916bedbb7b8768ca3103cccc
Karen <kareenmora66@gmail.com>
2019-03-28 18:56:15 -0500

    Iniciando proyecto

---
+++ pages/contacto.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Desde el contacto</h1>
+    </div>
+</template>
+
+<script>
+import navbar from '../components/navbar'
+export default {
+    components: {navbar}
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>holi {{ msg }}</h1>
+        <input type="text" v-model="msg">
+        <button @click="guerdar" class="btn btn-primary">Guardar</button>
+        <b-button variant="primary">Guardar</b-button>
+        <table>
+            <tr>
+                <th>Nombre</th>
+            </tr>
+            <tr v-for="(item, index) in municipios" :key="index">
+                <td>{{item}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+import navbar from '../components/navbar'
+//import contacto from './contacto'
+
+export default {
+    components: {navbar},
+    data(){
+        return{
+            msg: "Holi",
+            municipios: ['Mocoa', 'Villa', 'Puerto']
+        };
+    },
+    methods: {
+        guerdar(){
+            this.municipios.push(this.msg)
+            this.msg = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
